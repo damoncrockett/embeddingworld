238559d5bac820351d1da19b1962c174d6cb081f
damoncrockett <damoncrockett@hotmail.com>
2024-03-06 15:36:23 -0500

    hidden base toggle modal

--- src/components/BasemapToggles.js
+++ src/components/BasemapToggles.js
@@ -1,28 +1,35 @@
-// BasemapToggles.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const BasemapToggles = ({ basemaps, onToggle }) => {
+    const [isOpen, setIsOpen] = useState(false); 
+
     const handleChange = (event) => {
         const { name, checked } = event.target;
         onToggle(name, checked);
     };
 
+    const toggleModal = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div id="basemap-toggle-container">
-            {Object.keys(basemaps).map((basemapName) => (
-                <label key={basemapName} className="basemap-toggle-label">
-                    <input
-                        type="checkbox"
-                        name={basemapName}
-                        className="basemap-toggle-checkbox"
-                        onChange={handleChange}
-                    />
-                    {basemapName}
-                </label>
-            ))}
-        </div>
+        <>
+            <div id="basemap-toggle-container" className={isOpen ? 'open' : ''}>
+                {Object.keys(basemaps).map((basemapName) => (
+                    <label key={basemapName} className="basemap-toggle-label">
+                        <input
+                            type="checkbox"
+                            name={basemapName}
+                            className="basemap-toggle-checkbox"
+                            onChange={handleChange}
+                        />
+                        {basemapName.toUpperCase()}
+                    </label>
+                ))}
+                <div title='pre-made basemaps' className={`toggle-button ${isOpen ? 'open' : ''}`} onClick={toggleModal}>← </div>
+            </div>
+        </>
     );
 };
-
 
 export default BasemapToggles;
